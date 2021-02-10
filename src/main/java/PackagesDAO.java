13ccef84c5fe77aa465269dde6d762631a5c8d4b
Karolina <250013@student.pwr.edu.pl>
2021-02-10 20:57:39 +0100

    usuniete klasy

--- src/main/java/PackagesDAO.java
+++ src/main/java/PackagesDAO.java
@@ -18,26 +18,6 @@ public class PackagesDAO {
     public PackagesDAO() {
     }
 
-    private ObservableList<Clients> getClientsList(ResultSet rs) throws SQLException {
-
-        ObservableList<Clients> clientList = FXCollections.observableArrayList();
-
-        while (rs.next()) {
-
-            Clients c = new Clients();
-            c.setIdClient(rs.getInt("idClient"));
-            c.setImieNazwisko(rs.getString("ImieNazwisko"));
-            c.setAdres(rs.getString("adres"));
-            c.setEmail(rs.getString("email"));
-            c.setMobile_number(rs.getInt("mobile_number"));
-
-            clientList.add(c);
-            //clientList.add(a);
-        }
-
-        return clientList;
-    }
-
     private ObservableList<ClientBase> getClientBaseList(ResultSet rs) throws SQLException {
 
         ObservableList<ClientBase> clientBaseList = FXCollections.observableArrayList();
@@ -113,7 +93,7 @@ public class PackagesDAO {
 
 
             packageList.add(p);
-        //clientList.add(a);
+
         }
 
         return packageList;
@@ -146,50 +126,7 @@ public class PackagesDAO {
 
 
 
-/*
-    public ObservableList<Racket> searchRackets(String manuf) throws SQLException, ClassNotFoundException {
 
-        String selectStmt = "SELECT * FROM rackets WHERE manufacturer LIKE '%" + manuf + "%';";
-
-        try {
-
-            ResultSet resultSet = dbUtil.dbExecuteQuery(selectStmt);
-
-            ObservableList<Racket> rackets = getRacketList(resultSet);
-
-            consoleTextArea.appendText(selectStmt + "\n");
-
-            return rackets;
-
-        } catch (SQLException e) {
-            consoleTextArea.appendText("While searching a racket from '" + manuf + "' name, an error occurred. \n");
-            throw e;
-        }
-
-    }
-
- */
-
-    public ObservableList<Clients> showAllClients() throws SQLException, ClassNotFoundException {
-
-        String selectStmt = "SELECT * FROM clients;";
-
-        try {
-
-            ResultSet resultSet = dbUtil.dbExecuteQuery(selectStmt);
-
-            ObservableList<Clients> clientList = getClientsList(resultSet);
-            consoleTextArea.appendText(selectStmt);
-
-            return clientList;
-
-
-        } catch (SQLException e) {
-            consoleTextArea.appendText("While searching clients, an error occurred. \n");
-            throw e;
-        }
-
-    }
 
     public ObservableList<SenderView> showAllSender (String idName) throws SQLException, ClassNotFoundException {
 
@@ -356,27 +293,6 @@ public class PackagesDAO {
     }
 
 
-/*
-    public void insertClient(String name) throws SQLException, ClassNotFoundException {
-
-        StringBuilder sb = new StringBuilder("INSERT INTO rackets(model) VALUES('");
-        sb.append(name);
-        sb.append("');");
-        String insertStmt = sb.toString();
-
-        try {
-
-            dbUtil.dbExecuteUpdate(insertStmt);
-            consoleTextArea.appendText(insertStmt + "\n");
-
-        } catch (SQLException e) {
-            consoleTextArea.appendText("Error occurred while INSERT Operation.");
-            throw e;
-        }
-
-    }
-
- */
 
 
 
